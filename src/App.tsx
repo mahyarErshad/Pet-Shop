c43ff76a0a1c80d41e009f18e69af13fe1565336
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-07-17 19:33:01 +0430

    added reset css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './Style/Theme/theme';
+import React from "react";
+
+// MUI Utils
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./Style/Theme/theme";
+
+// CSS
+import "./Style/ResetCSS/reset.css";
 
 function App() {
   return (
     <>
-    <ThemeProvider theme={theme}>
-    <h1>Hello World</h1>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <h1>Hello World</h1>
+      </ThemeProvider>
     </>
   );
 }
