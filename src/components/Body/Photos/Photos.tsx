43c7cc70695900fd1c3297030177c5962eaf9b64
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-07-20 21:52:49 +0430

    worked on some animations

--- src/components/Body/Photos/Photos.tsx
+++ src/components/Body/Photos/Photos.tsx
@@ -22,16 +22,16 @@ function Photos() {
     <>
       <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100%", flexWrap: "wrap", flexDirection: "row-reverse" }}>
         <LightTooltip title="خرید لوازم گربه" TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} followCursor>
-          <Box sx={{ height: "31.25rem", width: { sm: "100%", md: "50%", lg: "25%" } }} component="img" src={cat} alt="cat" />
+          <Box sx={{ height: "31.25rem", width: { sm: "100%", md: "50%", lg: "25%" }, cursor: "pointer", "&:hover": { transform: "scale(1.1,1.1)", borderRadius: "16px", border: "5px solid #005B9F" } }} component="img" src={cat} alt="cat" />
         </LightTooltip>
         <LightTooltip title="خرید لوازم سگ" TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} followCursor>
-          <Box sx={{ height: "31.25rem", width: { sm: "100%", md: "50%", lg: "25%" } }} component="img" src={dog} alt="dog" />
+          <Box sx={{ height: "31.25rem", width: { sm: "100%", md: "50%", lg: "25%" }, cursor: "pointer", "&:hover": { transform: "scale(1.1,1.1)", borderRadius: "16px", border: "5px solid #005B9F" } }} component="img" src={dog} alt="dog" />
         </LightTooltip>
         <LightTooltip title="خرید لوازم پرندگان" TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} followCursor>
-          <Box sx={{ height: "31.25rem", width: { sm: "100%", md: "50%", lg: "25%" } }} component="img" src={bird} alt="bird" />
+          <Box sx={{ height: "31.25rem", width: { sm: "100%", md: "50%", lg: "25%" }, cursor: "pointer", "&:hover": { transform: "scale(1.1,1.1)", borderRadius: "16px", border: "5px solid #005B9F" } }} component="img" src={bird} alt="bird" />
         </LightTooltip>
         <LightTooltip title="خرید لوازم جوندگان" TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} followCursor>
-          <Box sx={{ height: "31.25rem", width: { sm: "100%", md: "50%", lg: "25%" } }} component="img" src={pig} alt="rodents" />
+          <Box sx={{ height: "31.25rem", width: { sm: "100%", md: "50%", lg: "25%" }, cursor: "pointer", "&:hover": { transform: "scale(1.1,1.1)", borderRadius: "16px", border: "5px solid #005B9F" } }} component="img" src={pig} alt="rodents" />
         </LightTooltip>
       </Box>
     </>
