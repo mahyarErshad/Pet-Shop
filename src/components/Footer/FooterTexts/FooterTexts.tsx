a98e6b66a78f2c3194ca3fe36019d86b4f52ea9e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-07-23 22:10:33 +0430

    footer menues are complete

---
+++ src/components/Footer/FooterTexts/FooterTexts.tsx
@@ -0,0 +1,45 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import React from "react";
+
+function FooterTexts() {
+  const theme = useTheme();
+  const fontStyle = {
+    fontSize: "0.8rem",
+    color: "#4A5F73",
+    fontWeight: "bold",
+    cursor: "pointer",
+    direction: "rtl",
+    marginRight: {
+      xs: "1rem",
+      md: "3rem",
+      lg: "5rem",
+    },
+    "&:hover": {
+      color: theme.palette.secondary.main,
+    },
+  };
+  const paragraphStyle = {
+    fontSize: "1rem",
+    color: "#4A5F73",
+    fontWeight: "normal",
+    direction: "rtl",
+    width: "60%",
+    textAlign: "justify",
+    textJustify: "inter - word",
+  };
+  return (
+    <>
+      <Box sx={{ p: "1rem", mt: "1.5rem", mb: "1.5rem", width: "100%", display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", alignItems: "flex-start", justifyContent: "space-between", gap: "0.5rem" }}>
+        <Box sx={{ width: "30%", display: "flex", flexDirection: "column", alignItems: "flex-end", justifyContent: "center", gap: "0.7rem" }}>
+          <Typography sx={fontStyle}>رویه ارسال سفارش</Typography>
+          <Typography sx={fontStyle}>شیوه های پرداخت</Typography>
+          <Typography sx={fontStyle}>قوانین و مقررات سایت</Typography>
+          <Typography sx={fontStyle}>تماس با ما</Typography>
+        </Box>
+        <Typography sx={paragraphStyle}>پت استور به عنوان یکی از قدیمی‌ترین پت شاپ های اینترنتی با بیش از 3000 هزار محصول ایرانی و خارجی آماده پاسخگویی به همه ی نیازهای پت شما هست. پت شاپ پت استور، ویترینی از غذای سگ و غذای گربه با برندهای معتبر مانند: رویال کنین، جوسرا و .. همراه با طیف وسیعی از لوازم جانبی برای پت شما است. کالای مورد نیاز پت خود را میتوانید با چند کلیک انتخاب کنید و در سریع ترین زمان ممکن درب منزل تحویل بگیرید. همچنین با مطالعه مطالب و ویدیوهای آموزشی در وبلاگ پت استور میتوانید راههای نگهداری از سگ و گربه خود را آموزش ببینید.</Typography>
+      </Box>
+    </>
+  );
+}
+
+export default FooterTexts;
--- src/components/Footer/Main/Footer.tsx
+++ src/components/Footer/Main/Footer.tsx
@@ -1,13 +1,15 @@
 import { Box, useTheme } from "@mui/material";
 import React from "react";
 import FooterPhotos from "../FooterPhotos/FooterPhotos";
+import FooterTexts from "../FooterTexts/FooterTexts";
 
 function Footer() {
   const theme = useTheme();
   return (
     <>
-      <Box sx={{ borderTop: `0.0625rem solid ${theme.palette.secondary.main}`, mt: "0.5rem", backgroundColor: theme.palette.primary.main, p: "0.5rem", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <Box sx={{ backgroundColor: "#ECEFF1", borderTop: `0.0625rem solid ${theme.palette.secondary.main}`, mt: "0.5rem", p: "0.5rem", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
         <FooterPhotos />
+        <FooterTexts />
       </Box>
     </>
   );
