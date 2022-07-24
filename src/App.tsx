316f5379c7ad964730561ad967ecd05a76911100
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-07-25 01:57:18 +0430

    Fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Register from "./pages/Register/Register/Register";
 
 // CSS
 import "./Style/ResetCSS/reset.css";
+import "./Style/global/style.css";
 
 // utils
 import { BrowserRouter, Route, Routes } from "react-router-dom";
