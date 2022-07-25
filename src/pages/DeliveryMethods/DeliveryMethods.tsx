656be6677b7e7a99dee0def3ed722aaefbd47fdc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-07-25 21:56:46 +0430

    Changed some styles to have a better UI

--- src/pages/DeliveryMethods/DeliveryMethods.tsx
+++ src/pages/DeliveryMethods/DeliveryMethods.tsx
@@ -1,8 +1,10 @@
+import { useTheme } from "@mui/system";
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import delivery from "../../images/delivery.jpg";
 
 function DeliveryMethods() {
+  const theme = useTheme();
   document.title = "روش های ارسال";
   const fontStyle = {
     fontSize: "0.875rem",
@@ -11,6 +13,10 @@ function DeliveryMethods() {
     textJustify: "inter - word",
     direction: "rtl",
     mt: "0.5rem",
+    lineHeight: "1.5rem",
+  };
+  const strongStyle = {
+    color: theme.palette.secondary.main,
   };
   return (
     <>
@@ -18,23 +24,33 @@ function DeliveryMethods() {
         <Box component="img" src={delivery} alt="delivery" sx={{ mb: "0.5rem", alignSelf: "center" }} />
         <Typography sx={fontStyle}>
           {" "}
-          <strong>هزینه ارسال: </strong>هزینه ارسال برای خرید بالای ۵۰۰ هزار تومان به سراسرکشور <strong>رایگان</strong> است و برای خرید های کم تر از ۵۰۰ هزار تومان ۲۹ هزار تومان می باشد (وزن و ابعاد بسته اهمیتی ندارد).
+          <strong style={strongStyle}>هزینه ارسال: </strong>
+          <br />
+          هزینه ارسال برای خرید بالای ۵۰۰ هزار تومان به سراسرکشور <strong style={strongStyle}>رایگان</strong> است و برای خرید های کم تر از ۵۰۰ هزار تومان ۲۹ هزار تومان می باشد (وزن و ابعاد بسته اهمیتی ندارد).
         </Typography>
         <Typography sx={fontStyle}>
           {" "}
-          <strong>زمان تحویل عادی در تهران: </strong>براساس انتخاب شما مشتری عزیز است. شما هنگام ثبت سفارش روز و ساعت تحویل سفارش را میتوانید انتخاب کنید.
+          <strong style={strongStyle}>زمان تحویل عادی در تهران: </strong>
+          <br />
+          براساس انتخاب شما مشتری عزیز است. شما هنگام ثبت سفارش روز و ساعت تحویل سفارش را میتوانید انتخاب کنید.
         </Typography>
         <Typography sx={fontStyle}>
           {" "}
-          <strong>زمان تحویل فوری در تهران: </strong>در صورتی که قصد داشتید همان ساعت سفارش شما ارسال شود کافیست گزینه ارسال فوری را انتخاب کنید تا با اسنپ یا تپ سی ارسال شود توجه داشته باشید در صورت ارسال فوری ۲ ساعته با اسنپ شرط ارسال رایگان برقرار نیست.
+          <strong style={strongStyle}>زمان تحویل فوری در تهران: </strong>
+          <br />
+          در صورتی که قصد داشتید همان ساعت سفارش شما ارسال شود کافیست گزینه ارسال فوری را انتخاب کنید تا با اسنپ یا تپ سی ارسال شود توجه داشته باشید در صورت ارسال فوری ۲ ساعته با اسنپ شرط ارسال رایگان برقرار نیست.
         </Typography>
         <Typography sx={fontStyle}>
           {" "}
-          <strong>زمان تحویل پست برای سایر شهرها: </strong>میانگین ۲ تا ۵ روز کاری از سوی اداره پست جمهوری اسلامی ایران می باشد (تمامی بسته ها به صورت پیشتاز ارسال می شوند).
+          <strong style={strongStyle}>زمان تحویل پست برای سایر شهرها: </strong>
+          <br />
+          میانگین ۲ تا ۵ روز کاری از سوی اداره پست جمهوری اسلامی ایران می باشد (تمامی بسته ها به صورت پیشتاز ارسال می شوند).
         </Typography>
         <Typography sx={fontStyle}>
           {" "}
-          <strong>زمان تحویل چاپار / تیپاکس برای سایر شهرها: </strong>بین ۱ الی ۳ روز کاری است به وسیله پست خصوصی به سفارش مشتری ارسال می شود توجه داشته باشید در این روش شرایط ارسال رایگان برقرار نیست.
+          <strong style={strongStyle}>زمان تحویل چاپار / تیپاکس برای سایر شهرها: </strong>
+          <br />
+          بین ۱ الی ۳ روز کاری است به وسیله پست خصوصی به سفارش مشتری ارسال می شود توجه داشته باشید در این روش شرایط ارسال رایگان برقرار نیست.
         </Typography>
       </Box>
     </>
