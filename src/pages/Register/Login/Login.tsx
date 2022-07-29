dd7638484e62e18ffaa6c97e59f45a12332a1d7c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-07-30 02:31:27 +0430

    Login page has a component Button

--- src/pages/Register/Login/Login.tsx
+++ src/pages/Register/Login/Login.tsx
@@ -1,20 +1,15 @@
-import { Typography, FormControl, Button, ButtonBase } from "@mui/material";
+import { FormControl } from "@mui/material";
 import RegisterInputs from "../../../components/Utils/Inputs/RegisterInputs/RegisterInputs";
+import RegisterButton from "../../../components/Utils/Buttons/RegisterButtons/RegisterButton";
 import React from "react";
 
 function Login() {
-  const inputStyle = { width: { lg: "50%", md: "50%", xs: "75%" } };
-
   return (
     <>
       <FormControl sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "1rem" }}>
         <RegisterInputs label="Email" type="text" />
         <RegisterInputs label="Password" type="Password" />
-        <ButtonBase sx={inputStyle}>
-          <Button sx={{ p: "0.5rem", width: "100%" }} variant="contained" color="secondary">
-            <Typography sx={{ textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }}>ورود</Typography>
-          </Button>
-        </ButtonBase>
+        <RegisterButton label="ورود" />
       </FormControl>
     </>
   );
