8a5dfd7c05f0326515ff571a3710ff01efdd94f1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-07-31 19:45:16 +0430

    Cards has been styled

--- src/components/Utils/Cards/Cards.tsx
+++ src/components/Utils/Cards/Cards.tsx
@@ -3,26 +3,34 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions } from "@mui/material";
+import { Box, CardActionArea, CardActions } from "@mui/material";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
 
 export default function Cards() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ width: { lg: "30%", md: "27%", sm: "40%", xs: "80%" } }}>
       <CardActionArea>
-        <CardMedia component="img" height="140" image="/static/images/cards/contemplative-reptile.jpg" alt="green iguana" />
+        <CardMedia sx={{ objectFit: "contain", p: "0.5rem" }} component="img" height="200" image="https://eurovet.cdn.shoprenter.hu/custom/eurovet/image/cache/w2277h1500wt1/product/%C3%A1llateledel%20k%C3%A9pei/Royal/xydkeqcnmzk80dqsjeli.png?lastmod=1657093064.1490595398" alt="green iguana" />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography dir="rtl" gutterBottom variant="h5" component="div">
             Lizard
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography sx={{ fontSize: "0.875rem" }} dir="rtl" variant="body2" color="#000">
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
+        <ButtonGroup disableElevation variant="contained" color="secondary">
+          <Button>+</Button>
+          <Box sx={{ width: "2.5612rem", height: "2.2813rem", backgroundColor: "#ECEFF1", display: "flex", justifyContent: "center", alignItems: "center", border: "1px solid #000" }}>
+            <Typography variant="h5" color="#000">
+              ۰
+            </Typography>
+          </Box>
+          <Button>-</Button>
+        </ButtonGroup>
       </CardActions>
     </Card>
   );
--- src/pages/Products/Main/Products.tsx
+++ src/pages/Products/Main/Products.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import React from "react";
+import Cards from "../../../components/Utils/Cards/Cards";
 import ProductsHeader from "../ProductsHeader/ProductsHeader";
 
 function Products() {
@@ -7,6 +8,14 @@ function Products() {
     <>
       <Box>
         <ProductsHeader />
+        <Box sx={{ mb: "1rem", width: "100%", px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem", flexWrap: "wrap" }}>
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+        </Box>
       </Box>
     </>
   );
