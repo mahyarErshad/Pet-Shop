88bdc27e5bdbc6608db764ff52b65e46d0a42594
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-07-31 20:45:42 +0430

    Changed the name to Paginations to avoid conflict with MUI Pagination

--- src/components/Utils/Paginations/Paginations.tsx
+++ src/components/Utils/Paginations/Paginations.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 
-export default function Pagination() {
+export default function Paginations() {
   return (
     <>
       <Stack spacing={2}>
