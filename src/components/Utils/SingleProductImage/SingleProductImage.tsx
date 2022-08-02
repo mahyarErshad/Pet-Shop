6139f7b791049874f4680734dfaa1d7b7c6f2d95
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-02 23:56:18 +0430

    Added container Defined MouseEnter/Leave/Move functions for container

--- src/components/Utils/SingleProductImage/SingleProductImage.tsx
+++ src/components/Utils/SingleProductImage/SingleProductImage.tsx
@@ -1,14 +1,28 @@
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 interface props {
   image: string;
 }
 
 function SingleProductImage(props: props) {
   const image = props.image;
+  const [style, setStyle] = useState(true);
+  const onMouseEnterHandler = function (event: any) {
+    update(event);
+  };
+  const onMouseLeaveHandler = function () {
+    setStyle(false);
+  };
+  const onMouseMoveHandler = function (event: any) {
+    if (isTimeToUpdate()) {
+      update(event);
+    }
+  };
   return (
     <>
-      <Box component="img" src={image} alt="تصویر محصول" sx={{ width: { xs: "100%", md: "50%" }, minHeight: "15.625rem", maxHeight: "31.25rem", objectFit: "scale-down" }} />
+      <Box onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} onMouseMove={onMouseMoveHandler} sx={{ width: { backgroundColor: "red", perspective: "40px", xs: "95%", md: "45%" }, minHeight: "15.625rem", maxHeight: "31.25rem", p: "0", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Box component="img" src={image} alt="تصویر محصول" sx={{ width: "100%", height: "100%", objectFit: "scale-down" }} />
+      </Box>
     </>
   );
 }
