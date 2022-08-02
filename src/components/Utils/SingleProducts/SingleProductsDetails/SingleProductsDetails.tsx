8ffcff4c809fc0ae366213581454121bb032d251
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 01:38:33 +0430

    SingleProduct has a 0.5rem gap

--- src/components/Utils/SingleProducts/SingleProductsDetails/SingleProductsDetails.tsx
+++ src/components/Utils/SingleProducts/SingleProductsDetails/SingleProductsDetails.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import React from "react";
 
 function SingleProductsDetails() {
-  return <Box sx={{ width: { backgroundColor: "red", xs: "95%", md: "45%" }, minHeight: "15.625rem", maxHeight: "31.25rem", p: "0.7rem", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}></Box>;
+  return <Box sx={{ width: { backgroundColor: "pink", xs: "95%", md: "45%" }, minHeight: "15.625rem", maxHeight: "31.25rem", p: "0.7rem", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}></Box>;
 }
 
 export default SingleProductsDetails;
--- src/pages/SingleProduct/SingleProduct.tsx
+++ src/pages/SingleProduct/SingleProduct.tsx
@@ -5,7 +5,7 @@ import SingleProductsDetails from "../../components/Utils/SingleProducts/SingleP
 
 function SingleProduct() {
   return (
-    <Box sx={{ width: "100%", display: "flex", flexDirection: "column", px: { xs: "0.5rem", md: "1rem", alignItems: "center", justifyContent: "center" } }}>
+    <Box sx={{ width: "100%", display: "flex", flexDirection: "column", px: { xs: "0.5rem", md: "1rem", alignItems: "center", justifyContent: "center",gap:"0.5rem" } }}>
       <Box sx={{ width: "100%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", flexWrap: "wrap" }}>
         <SingleProductImage image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" />
         <SingleProductsDetails />
