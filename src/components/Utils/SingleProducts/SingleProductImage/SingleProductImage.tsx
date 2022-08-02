376d1d3a4836382898d5884ca9e21c50584b1d45
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 02:09:02 +0430

    Changed sm width and height

--- src/components/Utils/SingleProducts/SingleProductImage/SingleProductImage.tsx
+++ src/components/Utils/SingleProducts/SingleProductImage/SingleProductImage.tsx
@@ -8,7 +8,7 @@ function SingleProductImage(props: props) {
   const image = props.image;
   return (
     <>
-      <Box sx={{ backgroundColor: "red", width: { xs: "75%", sm: "50%", md: "48%" }, height: { xs: "15.625rem", md: "31.25rem" }, p: "0", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "8px" }}>
+      <Box sx={{ backgroundColor: "red", width: { xs: "75%", sm: "65%", md: "48%" }, height: { xs: "15.625rem", sm: "21.875rem", md: "31.25rem" }, p: "0", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "8px" }}>
         <Box component="img" src={image} alt="تصویر محصول" sx={{ width: "100%", height: "100%", objectFit: "scale-down", borderRadius: "16px" }} />
       </Box>
     </>
