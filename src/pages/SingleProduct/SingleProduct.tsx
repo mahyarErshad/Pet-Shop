220e91237e2ddf6eb40baa7c41c762790712462e
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-02 23:07:40 +0430

    This is the wrapper for Component's header

--- src/pages/SingleProduct/SingleProduct.tsx
+++ src/pages/SingleProduct/SingleProduct.tsx
@@ -2,7 +2,11 @@ import { Box } from "@mui/material";
 import React from "react";
 
 function SingleProduct() {
-  return <Box sx={{ width: "100%", display: "flex", flexDirection: "column", px: { xs: "0.5rem", md: "1rem", alignItems: "center", justifyContent: "center" } }}></Box>;
+  return (
+    <Box sx={{ width: "100%", display: "flex", flexDirection: "column", px: { xs: "0.5rem", md: "1rem", alignItems: "center", justifyContent: "center" } }}>
+      <Box sx={{ width: "100%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", flexWrap: "wrap" }}></Box>
+    </Box>
+  );
 }
 
 export default SingleProduct;
