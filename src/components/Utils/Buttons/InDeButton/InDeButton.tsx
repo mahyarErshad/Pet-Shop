9c369083d2d323c8026c0f3f4df53cad6379d0d2
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 02:29:29 +0430

    InDeButton now accepts width as string or the width is "2.5612rem"

--- src/components/Utils/Buttons/InDeButton/InDeButton.tsx
+++ src/components/Utils/Buttons/InDeButton/InDeButton.tsx
@@ -3,12 +3,17 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
 import { Box, Typography } from "@mui/material";
 
-function InDeButton() {
+interface props {
+  width?: string;
+}
+
+function InDeButton(props: props) {
+  const width = props.width || "2.5612rem";
   return (
     <>
       <ButtonGroup disableElevation variant="contained" color="secondary">
         <Button>+</Button>
-        <Box sx={{ width: "2.5612rem", height: "2.2813rem", backgroundColor: "#ECEFF1", display: "flex", justifyContent: "center", alignItems: "center", border: "1px solid #000" }}>
+        <Box sx={{ width, height: "2.2813rem", backgroundColor: "#ECEFF1", display: "flex", justifyContent: "center", alignItems: "center", border: "1px solid #000" }}>
           <Typography variant="h5" color="#000">
             ۰
           </Typography>
