f8e876dad5dd5568fe14beb4ea5c0cad686a3b7a
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 01:25:25 +0430

    SingleProduct's image is ready to use as component

--- src/components/Utils/SingleProductImage/SingleProductImage.tsx
+++ src/components/Utils/SingleProductImage/SingleProductImage.tsx
@@ -1,32 +1,15 @@
 import { Box } from "@mui/system";
-import React, { useState } from "react";
+import React from "react";
 interface props {
   image: string;
 }
 
 function SingleProductImage(props: props) {
   const image = props.image;
-  const [style, setStyle] = useState(true);
-  const counter = 0;
-  const updateRate = 10;
-  const isTimeToUpdate = function () {
-    return counter++ % updateRate === 0;
-  };
-  const onMouseEnterHandler = function (event: any) {
-    update(event);
-  };
-  const onMouseLeaveHandler = function () {
-    setStyle(false);
-  };
-  const onMouseMoveHandler = function (event: any) {
-    if (isTimeToUpdate()) {
-      update(event);
-    }
-  };
   return (
     <>
-      <Box onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} onMouseMove={onMouseMoveHandler} sx={{ width: { backgroundColor: "red", perspective: "40px", xs: "95%", md: "45%" }, minHeight: "15.625rem", maxHeight: "31.25rem", p: "0", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Box component="img" src={image} alt="تصویر محصول" sx={{ width: "100%", height: "100%", objectFit: "scale-down" }} />
+      <Box sx={{ width: { backgroundColor: "red", perspective: "40px", xs: "95%", md: "45%" }, minHeight: "15.625rem", maxHeight: "31.25rem", p: "0", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Box component="img" src={image} alt="تصویر محصول" sx={{ width: "100%", height: "100%", objectFit: "scale-down", borderRadius: "16px" }} />
       </Box>
     </>
   );
