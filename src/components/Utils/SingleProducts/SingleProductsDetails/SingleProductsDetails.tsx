b0d41d001e7e46d2655fb03785ea36a32928f945
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 01:36:19 +0430

    Added main container

--- src/components/Utils/SingleProducts/SingleProductsDetails/SingleProductsDetails.tsx
+++ src/components/Utils/SingleProducts/SingleProductsDetails/SingleProductsDetails.tsx
@@ -1,7 +1,8 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 function SingleProductsDetails() {
-  return <div>SingleProductsDetails</div>;
+  return <Box sx={{ width: { backgroundColor: "red", xs: "95%", md: "45%" }, minHeight: "15.625rem", maxHeight: "31.25rem", p: "0.7rem", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}></Box>;
 }
 
 export default SingleProductsDetails;
