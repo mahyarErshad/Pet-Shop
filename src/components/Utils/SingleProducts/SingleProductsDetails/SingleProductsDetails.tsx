8e5c1a7dd4d3c3406215c562719d0ebf2de371ec
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 02:13:23 +0430

    Title is done

--- src/components/Utils/SingleProducts/SingleProductsDetails/SingleProductsDetails.tsx
+++ src/components/Utils/SingleProducts/SingleProductsDetails/SingleProductsDetails.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function SingleProductsDetails() {
   return (
-    <Box dir="rtl" sx={{ backgroundColor: "pink", width: { xs: "90%", md: "48%" }, minHeight: "15.625rem", p: "0.7rem", display: "flex", justifyContent: "flex-start", alignItems: "flex-start", flexDirection: "column" }}>
+    <Box dir="rtl" sx={{ backgroundColor: "pink", width: { xs: "90%", md: "48%" }, minHeight: "15.625rem", p: "1rem", display: "flex", justifyContent: "flex-start", alignItems: "flex-start", flexDirection: "column" }}>
       <Typography dir="rtl" variant="h5" sx={{ fontSize: "1.5rem", fontWeight: "bold", mt: "1rem" }}>
         غذای خشک گربه پرشین رویال کنین
       </Typography>
