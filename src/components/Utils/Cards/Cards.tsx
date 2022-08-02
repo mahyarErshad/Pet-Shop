e697739b9bee5b34b2f7973e1467cd249e254930
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 03:06:18 +0430

    Imported globalStyle and fixed some bugs

--- src/components/Utils/Cards/Cards.tsx
+++ src/components/Utils/Cards/Cards.tsx
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 import { Box, CardActionArea, CardActions } from "@mui/material";
 import InDeButton from "../Buttons/InDeButton/InDeButton";
 import { Link } from "react-router-dom";
+import "../../../Style/global/style.css";
 
 interface Iprops {
   title: string;
@@ -21,11 +22,11 @@ export default function Cards(props: Iprops) {
   return (
     <>
       <Card sx={{ width: { lg: "30%", md: "27%", sm: "40%", xs: "80%" } }}>
-        <Link to="/id">
-          <CardActionArea>
+        <CardActionArea>
+          <Link className="noDecoration" to="/id">
             <CardMedia sx={{ objectFit: "contain", p: "0.5rem" }} component="img" height="200" image={image} alt={title} />
             <CardContent sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-              <Typography sx={{ fontSize: "1rem", fontWeight: "bold" }} dir="rtl" gutterBottom variant="h5" component="div">
+              <Typography sx={{ fontSize: "1rem", fontWeight: "bold", color: "#000" }} dir="rtl" gutterBottom variant="h5" component="div">
                 {title}
               </Typography>
               <Box sx={{ alignSelf: "flex-start", backgroundColor: "#D5C9E0", p: "0.5rem", borderRadius: "20px" }}>
@@ -34,11 +35,11 @@ export default function Cards(props: Iprops) {
                 </Typography>
               </Box>
             </CardContent>
-          </CardActionArea>
-          <CardActions sx={{ px: "1rem", py: "0.5rem" }}>
-            <InDeButton />
-          </CardActions>
-        </Link>
+          </Link>
+        </CardActionArea>
+        <CardActions sx={{ px: "1rem", py: "0.5rem" }}>
+          <InDeButton />
+        </CardActions>
       </Card>
     </>
   );
