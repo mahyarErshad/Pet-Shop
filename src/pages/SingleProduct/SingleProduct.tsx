4801737f01f0ec40cf3f1f499f0441749bdc8250
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-02 23:19:28 +0430

    Added an image. It's fully responsive and fits for maximum quality

--- src/pages/SingleProduct/SingleProduct.tsx
+++ src/pages/SingleProduct/SingleProduct.tsx
@@ -4,7 +4,9 @@ import React from "react";
 function SingleProduct() {
   return (
     <Box sx={{ width: "100%", display: "flex", flexDirection: "column", px: { xs: "0.5rem", md: "1rem", alignItems: "center", justifyContent: "center" } }}>
-      <Box sx={{ width: "100%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", flexWrap: "wrap" }}></Box>
+      <Box sx={{ width: "100%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", flexWrap: "wrap" }}>
+        <Box component="img" src="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" alt="تصویر محصول" sx={{ width: { xs: "100%", md: "50%" }, minHeight: "15.625rem", maxHeight: "31.25rem", objectFit: "scale-down" }} />
+      </Box>
     </Box>
   );
 }
