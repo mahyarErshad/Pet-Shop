83184a38104e231df89b3f2a2ca7a31c3570b3de
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 01:54:06 +0430

    Changed some styles

--- src/components/Utils/SingleProducts/SingleProductImage/SingleProductImage.tsx
+++ src/components/Utils/SingleProducts/SingleProductImage/SingleProductImage.tsx
@@ -8,7 +8,7 @@ function SingleProductImage(props: props) {
   const image = props.image;
   return (
     <>
-      <Box sx={{ width: { backgroundColor: "red", perspective: "40px", xs: "75%", md: "48%" }, height: { xs: "15.625rem", md: "31.25rem" }, p: "0", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Box sx={{ backgroundColor: "red", width: { xs: "75%", sm: "50%", md: "48%" }, height: { xs: "15.625rem", md: "31.25rem" }, p: "0", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "8px" }}>
         <Box component="img" src={image} alt="تصویر محصول" sx={{ width: "100%", height: "100%", objectFit: "scale-down", borderRadius: "16px" }} />
       </Box>
     </>
