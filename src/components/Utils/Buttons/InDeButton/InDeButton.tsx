a49a1c629c577245e898120192dbe7d74ae116a4
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 18:40:29 +0430

    InDeButton accepts fontSize as prop

--- src/components/Utils/Buttons/InDeButton/InDeButton.tsx
+++ src/components/Utils/Buttons/InDeButton/InDeButton.tsx
@@ -6,18 +6,20 @@ import { Box, Typography } from "@mui/material";
 interface props {
   width?: string;
   height?: string;
+  fontSize?: string;
 }
 
 function InDeButton(props: props) {
   const screen = props.width || "2.5612rem";
   const buttons = props.width || "";
   const height = props.height || "2.2813rem";
+  const fontSize = props.fontSize || "2rem";
   return (
     <>
       <ButtonGroup disableElevation variant="contained" color="secondary">
         <Button sx={{ width: buttons }}>+</Button>
         <Box sx={{ width: screen, height, backgroundColor: "#ECEFF1", display: "flex", justifyContent: "center", alignItems: "center", borderTop: "1px solid #000", borderBottom: "1px solid #000" }}>
-          <Typography sx={{ fontSize: "2rem" }} variant="h5" color="#000">
+          <Typography sx={{ fontSize }} variant="h5" color="#000">
             ۰
           </Typography>
         </Box>
