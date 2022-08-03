7adc6dc23cdd48537f22e6ea082cb9b2b2e128af
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 19:23:48 +0430

    Description is responsive and styled

--- src/components/Utils/SingleProducts/SingleProductsDescription/SingleProductsDescription.tsx
+++ src/components/Utils/SingleProducts/SingleProductsDescription/SingleProductsDescription.tsx
@@ -2,12 +2,38 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 
 function SingleProductsDescription() {
+  const paragraphStyle = {
+    fontSize: "1rem",
+    fontWeight: "normal",
+    width: "100%",
+    textAlign: "justify",
+    textJustify: "inter - word",
+    lineHeight: "1.5rem",
+  };
   return (
     <>
-      <Box sx={{ width: "100%", display: "flex", px: "1rem", justifyContent: "flex-end", mt: "1rem" }}>
+      <Box sx={{ width: "100%", display: "flex", px: "1rem", justifyContent: "flex-end", mt: "1rem", flexDirection: "column", gap: "1rem" }}>
         <Typography dir="rtl" sx={{ fontSize: "1.5rem", fontWeight: "bold" }} variant="h5">
           توضیحات محصول:
         </Typography>
+        <Box sx={{ width: "100%", px: "1.5rem" }}>
+          <Typography dir="rtl" sx={paragraphStyle}>
+            • سلامت پوست و موی گربه
+            <br />
+            • کاهش و کنترل هربال (تجمع گلوله مو در دستگاه گوارش)
+            <br />
+            • غذای کامل برای گربه های پرشین بالغ (بزرگتر از 12 ماه)
+            <br />
+            • حاوی غلات و عصاره پروتئین گیاهی
+            <br />
+            • حاوی تکه‌های لذیذ در شیره مغذی گوشت
+            <br />
+            • سرشار از مواد معدنی و مشتقات منشا گیاهی
+            <br />
+            • حاوی ویتامین‌های D3, E1, E2, E4 و عناصر منگنز و روی
+            <br />• دارای آنتی اکسیدان و ویتامین برای کمک به سیستم دفاعی
+          </Typography>
+        </Box>
       </Box>
     </>
   );
