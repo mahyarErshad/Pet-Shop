bb3de16f518db0ef890f9fad4db5f9f66f5f31c4
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 18:54:50 +0430

    Created SingleProductsDescription component

---
+++ src/components/Utils/SingleProducts/SingleProductsDescription/SingleProductsDescription.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SingleProductsDescription() {
+  return (
+    <>
+      <div>SingleProductsDescription</div>
+    </>
+  );
+}
+
+export default SingleProductsDescription;
