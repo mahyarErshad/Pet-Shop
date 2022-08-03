5de3f4e7b52ecff64b3440f3dd5ba718a4519651
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-03 19:10:13 +0430

    Added title for product description

--- src/components/Utils/SingleProducts/SingleProductsDescription/SingleProductsDescription.tsx
+++ src/components/Utils/SingleProducts/SingleProductsDescription/SingleProductsDescription.tsx
@@ -1,9 +1,14 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 function SingleProductsDescription() {
   return (
     <>
-      <div>SingleProductsDescription</div>
+      <Box sx={{ width: "100%", display: "flex", px: "1rem", justifyContent: "flex-end", mt: "1rem" }}>
+        <Typography dir="rtl" sx={{ fontSize: "1.5rem", fontWeight: "bold" }} variant="h5">
+          توضیحات محصول:
+        </Typography>
+      </Box>
     </>
   );
 }
--- src/pages/SingleProduct/SingleProduct.tsx
+++ src/pages/SingleProduct/SingleProduct.tsx
@@ -7,7 +7,7 @@ import SingleProductsDetails from "../../components/Utils/SingleProducts/SingleP
 function SingleProduct() {
   return (
     <Box sx={{ width: "100%", mb: "1rem", display: "flex", flexDirection: "column", px: { xs: "0.5rem", md: "1rem", alignItems: "center", justifyContent: "center", gap: "0.5rem" } }}>
-      <Box sx={{ width: "100%", display: "flex", flexDirection: "row-reverse", alignItems: { xs: "center", md: "flex-start" }, justifyContent: { xs: "center", md: "flex-start" }, flexWrap: "wrap" }}>
+      <Box sx={{ width: "100%", display: "flex", flexDirection: "row-reverse", alignItems: { xs: "center", md: "flex-start" }, justifyContent: { xs: "center", md: "flex-start" }, flexWrap: "wrap", borderBottom: "2px solid #ECEFF1", pb: "0.15rem" }}>
         <SingleProductImage image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" />
         <SingleProductsDetails title="غذای خشک گربه پرشین رویال کنین" price={1200000} balance={1000} fontSize="3rem" />
       </Box>
