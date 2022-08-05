fb1a2e5cd004465c1870f7de28a0823bdff7b987
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-05 16:43:53 +0430

    Copied everything from header

--- src/components/Utils/Buttons/Cart/Icon/CarIcon.tsx
+++ src/components/Utils/Buttons/Cart/Icon/CarIcon.tsx
@@ -1,7 +1,20 @@
+import { Badge, IconButton } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function CarIcon() {
-  return <div>CarIcon</div>;
+  return (
+    <>
+      <Link className="noDecoration" to="/cart">
+        <IconButton sx={{ margin: "0 0.5rem" }} size="large" aria-label="notifications" color="secondary">
+          <Badge badgeContent={5} color="error">
+            <ShoppingCartIcon color="secondary" fontSize="large" />
+          </Badge>
+        </IconButton>
+      </Link>
+    </>
+  );
 }
 
 export default CarIcon;
