83a4a4544ff3ee8aa7e2d1afca3e9e34f05c1907
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-05 16:17:35 +0430

    Fixed an error

--- src/components/Utils/Buttons/RegisterButtons/RegisterButton.tsx
+++ src/components/Utils/Buttons/RegisterButtons/RegisterButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, ButtonBase, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 interface IButton {
   title: string;
@@ -10,11 +10,9 @@ function RegisterButton(props: IButton) {
   const { title } = props;
   return (
     <>
-      <ButtonBase sx={inputStyle}>
-        <Button sx={{ p: "0.5rem", width: "100%", alignSelf: "flex-end" }} variant="contained" color="secondary">
-          <Typography sx={{ textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }}>{title}</Typography>
-        </Button>
-      </ButtonBase>
+      <Button sx={{ p: "0.5rem", width: { lg: "25%", md: "25%", xs: "37%" }, alignSelf: "flex-end" }} variant="contained" color="secondary">
+        <Typography sx={{ textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }}>{title}</Typography>
+      </Button>
     </>
   );
 }
