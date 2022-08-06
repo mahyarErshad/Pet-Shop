a136dbe35b787333808418f6584d84f73b241f98
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-07 03:06:19 +0430

    Removed "my" from styles because CartItems have a gap

--- src/components/Utils/CartItem/CartItem.tsx
+++ src/components/Utils/CartItem/CartItem.tsx
@@ -15,7 +15,7 @@ function CartItem(props: IProps) {
   const { image, title, price } = props;
   return (
     <>
-      <Box sx={{ width: { xs: "100%", sm: "100%", md: "70%", lg: "60%" }, my: "0.5rem", display: "flex", flexWrap: "wrap", flexDirection: "row-reverse", justifyContent: "space-between", alignItems: "center", gap: "0.5rem", borderRadius: "16px", border: "1px solid #ECEFF1", p: "0.5rem", alignSelf: "center" }}>
+      <Box sx={{ width: { xs: "100%", sm: "100%", md: "70%", lg: "60%" }, display: "flex", flexWrap: "wrap", flexDirection: "row-reverse", justifyContent: "space-between", alignItems: "center", gap: "0.5rem", borderRadius: "16px", border: "1px solid #ECEFF1", p: "0.5rem", alignSelf: "center" }}>
         <Box className="noDecoration" component={Link} to="/id" sx={{ width: { xs: "50%", sm: "40%", md: "50%" }, display: "flex", flexDirection: "row-reverse", gap: "0.2rem", justifyContent: "center", alignItems: "center" }}>
           <Box component="img" src={image} alt="تصویر محصول" sx={{ width: "3rem", height: "3rem", borderRadius: "50%" }} />
           <Typography dir="rtl" variant="h5" sx={{ fontSize: "1rem", color: "#000", fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
