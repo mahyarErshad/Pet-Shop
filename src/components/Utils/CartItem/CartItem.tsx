48f94d46ba2383d13517d54a1a03c01d14cffeed
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-07 02:38:35 +0430

    Imported Everything needed

--- src/components/Utils/CartItem/CartItem.tsx
+++ src/components/Utils/CartItem/CartItem.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 import InDeButton from "../Buttons/InDeButton/InDeButton";
 
 function CartItem() {
