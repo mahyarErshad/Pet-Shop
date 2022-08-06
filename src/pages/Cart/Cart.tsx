8643c477bfb06269a2e2245513fac361d7d79690
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-07 01:53:11 +0430

    Imported Link

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 import InDeButton from "../../components/Utils/Buttons/InDeButton/InDeButton";
 import MyButton from "../../components/Utils/Buttons/MyButton/MyButton";
 
@@ -12,12 +13,14 @@ function Cart() {
           <MyButton text="حذف همه" color="error" />
         </Box>
         <Box sx={{ width: { xs: "100%", sm: "100%", md: "70%", lg: "60%" }, display: "flex", flexWrap: "wrap", flexDirection: "row-reverse", justifyContent: "space-between", alignItems: "center", gap: "0.5rem", borderRadius: "16px", border: "1px solid #ECEFF1", p: "0.5rem", alignSelf: "center" }}>
-          <Box sx={{ width: { xs: "50%", sm: "40%", md: "50%" }, display: "flex", flexDirection: "row-reverse", gap: "0.2rem", justifyContent: "center", alignItems: "center" }}>
-            <Box component="img" src="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" alt="تصویر محصول" sx={{ width: "3rem", height: "3rem", borderRadius: "50%" }} />
-            <Typography dir="rtl" variant="h5" sx={{ fontSize: "1rem", fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
-              غذای خشک گربه پرشین رویال کنین
-            </Typography>
-          </Box>
+          <Link to="/id">
+            <Box sx={{ width: { xs: "50%", sm: "40%", md: "50%" }, display: "flex", flexDirection: "row-reverse", gap: "0.2rem", justifyContent: "center", alignItems: "center" }}>
+              <Box component="img" src="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" alt="تصویر محصول" sx={{ width: "3rem", height: "3rem", borderRadius: "50%" }} />
+              <Typography dir="rtl" variant="h5" sx={{ fontSize: "1rem", fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
+                غذای خشک گربه پرشین رویال کنین
+              </Typography>
+            </Box>
+          </Link>
           <Box sx={{ width: { xs: "40%", sm: "45%", md: "40%" }, display: "flex", flexDirection: { xs: "column-reverse", sm: "row-reverse" }, gap: "0.5rem", justifyContent: "center", alignItems: "center" }}>
             <InDeButton width="2.5rem" height="2rem" fontSize="1.2rem" />
             <Typography dir="rtl" variant="h5" sx={{ fontSize: "1rem", fontWeight: "normal", textAlign: "center", whiteSpace: "nowrap", overflow: "hidden" }}>
