762ff4a8f43c76ead0e18fbadc56d4ffd087227c
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-07 03:24:50 +0430

    Added payment button and fully styled it

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -8,8 +8,11 @@ function Cart() {
   return (
     <>
       <Box sx={{ width: "100%", px: "1rem", display: "flex", flexDirection: "column", gap: "1rem", my: "1rem" }}>
-        <Box sx={{ width: "100%", display: "flex", paddingLeft: "7.3rem" }}>
-          <MyButton text="حذف همه" color="error" />
+        <Box sx={{ width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "wrap" }}>
+          <Box sx={{ width: { xs: "100%", md: "30%" }, px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
+            <MyButton text="ثبت سفارش" />
+            <MyButton text="حذف همه" color="error" />
+          </Box>
         </Box>
         <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />
         <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />
