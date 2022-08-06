5c0ab17afec7fcd6a66711324baad4903adf7f2b
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-07 02:44:04 +0430

    Imported global css

--- src/components/Utils/CartItem/CartItem.tsx
+++ src/components/Utils/CartItem/CartItem.tsx
@@ -2,6 +2,7 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import InDeButton from "../Buttons/InDeButton/InDeButton";
+import "../../../Style/global/style.css";
 
 function CartItem() {
   return (
