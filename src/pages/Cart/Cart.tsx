4d52f3b0ee155fa671bd0343f3262b8de8440f7a
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-06 16:10:09 +0430

    Delete all button has set up

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,11 +1,16 @@
-import { Box, Button } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
+import MyButton from "../../components/Utils/Buttons/MyButton/MyButton";
 
 function Cart() {
   document.title = "پت شاپ فینیکس | سبد خرید";
   return (
     <>
-      <Box sx={{ width: "100%", px: "1rem", display: "flex", flexDirection: "column", gap: "1rem" }}></Box>
+      <Box sx={{ width: "100%", px: "1rem", display: "flex", flexDirection: "column", gap: "1rem", my: "1rem" }}>
+        <Box sx={{ width: "100%", display: "flex", paddingLeft: "7.3rem" }}>
+          <MyButton text="حذف همه" color="error" />
+        </Box>
+      </Box>
     </>
   );
 }
