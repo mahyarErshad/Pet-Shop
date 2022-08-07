eee5d40633d17e93b48702795a5753470da461d4
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-07 04:32:31 +0430

    CartOperationSection is working the same as component

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,26 +1,14 @@
-import { Box, Input, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
-import MyButton from "../../components/Utils/Buttons/MyButton/MyButton";
 import CartItem from "../../components/Utils/CartItem/CartItem";
-import { separateNumber } from "../../functions/functions";
+import CartOperationSection from "./CartOperationSection";
 
 function Cart() {
   document.title = "پت شاپ فینیکس | سبد خرید";
   return (
     <>
       <Box sx={{ width: "100%", px: "1rem", display: "flex", flexDirection: "column", gap: "1rem", my: "1rem" }}>
-        <Box sx={{ width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", flexWrap: "wrap", gap: "0.7rem" }}>
-          <Box sx={{ width: { xs: "100%", md: "30%" }, px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
-            <MyButton text="ثبت سفارش" />
-            <MyButton text="حذف همه" color="error" />
-          </Box>
-          <Box sx={{ width: { xs: "100%", md: "65%" }, flexDirection: { xs: "column", md: "row-reverse" }, px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
-            <Typography dir="rtl" sx={{ height: "2.5rem", borderRadius: "12px", px: "1rem", paddingTop: "0.6rem", paddingBottom: "2.5rem", fontSize: "1.2rem", fontWeight: "normal", backgroundColor: "#D5C9E0" }} variant="h5">
-              جمع کل: <strong>{separateNumber(3600000)} تومان</strong>
-            </Typography>
-            <Input dir="rtl" color="secondary" sx={{ width: "15.4475rem" }} placeholder="کد تخفیف دارید؟" />
-          </Box>
-        </Box>
+        <CartOperationSection />
         <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />
         <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />
         <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />
