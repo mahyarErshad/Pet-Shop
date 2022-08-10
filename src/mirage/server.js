52c510d9206d71a2d00f5228c37093045358b225
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-10 22:28:40 +0430

    Defined methods

--- src/mirage/server.js
+++ src/mirage/server.js
@@ -1,11 +1,78 @@
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
+import mockProducts from "./mockProduct";
+import mockCategories from "./mockCategory";
+import mockUsers from "./mockUser";
 
-export default function () {
-  createServer({
+export function makeServer({ environment = "test" } = {}) {
+  let server = createServer({
+    environment,
+
+    models: {
+      product: Model,
+      category: Model,
+      user: Model,
+    },
+
+    seeds(server) {
+      mockProducts.forEach((product) => {
+        server.create("product", {
+          title: product.title,
+          underTitle: product.underTitle,
+          description: product.description,
+          brand: product.brand,
+          category: product.category,
+          quantity: product.quantity,
+          image1: product.image1,
+          image2: product.image2,
+          image3: product.image3,
+          price: product.price,
+        });
+      });
+      mockCategories.forEach((category) => {
+        server.create("category", { name: category });
+      });
+      mockUsers.forEach((user) => {
+        server.create("user", {
+          username: user.username,
+          email: user.email,
+          password: user.password,
+          cart: user.cart,
+        });
+      });
+    },
+
     routes() {
-      this.get("/api/reminders", () => ({
-        reminders: [],
-      }));
+      this.namespace = "/api";
+
+      this.get("/products", (schema) => {
+        return schema.products.all();
+      });
+
+      this.get("/products/single/:id", (schema, request) => {
+        let id = request.params.id;
+        return schema.products.find(id);
+      });
+
+      this.get("/products/:categoryName", (schema, request) => {
+        let categoryName = request.params.categoryName;
+        return schema.products.where({ category: categoryName });
+      });
+
+      this.get("/categories", (schema) => {
+        return schema.categories.all();
+      });
+
+      this.get("/users/:username", (schema, request) => {
+        let userName = request.params.username;
+        return schema.users.find(userName);
+      });
+
+      this.post("/users", (schema, request) => {
+        let user = JSON.parse(request.requestBody);
+        return schema.users.create(user);
+      });
     },
   });
+
+  return server;
 }
