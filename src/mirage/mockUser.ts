a99fd499f64bb082a04c7cce93776576978ac1e3
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-10 22:33:46 +0430

    Defined first user

--- src/mirage/mockUser.ts
+++ src/mirage/mockUser.ts
@@ -1,14 +1,8 @@
 const mockUsers = [
   {
-    username: "alireza",
-    email: "alireza@gmail.com",
-    password: "alireza@01323202",
-    cart: [],
-  },
-  {
-    username: "mahdi",
-    email: "mahdi@gmail.com",
-    password: "mahdi@01323202",
+    username: "mahyar",
+    email: "mahyar.ershad92@gmail.com",
+    password: "123456",
     cart: [],
   },
 ];
