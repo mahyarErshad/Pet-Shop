67846917530ce5c0ace8fe63fcc5595079d9efad
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-10 22:30:08 +0430

    Added database for products, categories and users

---
+++ src/mirage/mockUser.ts
@@ -0,0 +1,16 @@
+const mockUsers = [
+  {
+    username: "alireza",
+    email: "alireza@gmail.com",
+    password: "alireza@01323202",
+    cart: [],
+  },
+  {
+    username: "mahdi",
+    email: "mahdi@gmail.com",
+    password: "mahdi@01323202",
+    cart: [],
+  },
+];
+
+export default mockUsers;
