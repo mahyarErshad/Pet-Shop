a70ad5e4db8cfc261b17c9c865abb72804bfde62
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-12 21:25:41 +0430

    Passed ID to cards

--- src/pages/Products/Main/Products.tsx
+++ src/pages/Products/Main/Products.tsx
@@ -38,7 +38,7 @@ function Products() {
       <>
         <Box>
           <ProductsHeader />
-          <Box sx={{ mb: "1rem", width: "100%", px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem", flexWrap: "wrap" }}>{products && products.map((product: any) => <Cards key={product.id} title={product.title} image={product.image} price={product.price} />)}</Box>
+          <Box sx={{ mb: "1rem", width: "100%", px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem", flexWrap: "wrap" }}>{products && products.map((product: any) => <Cards id={product.id} key={product.id} title={product.title} image={product.image} price={product.price} />)}</Box>
           <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", mb: "1rem" }}>
             <PaginationComponent page={10} />
           </Box>
