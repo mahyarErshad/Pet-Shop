8c9ae2db65e097adf7064d60d4f8411e2874fbdd
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-12 20:02:58 +0430

    Loading page is active while fetching data

--- src/pages/Products/Main/Products.tsx
+++ src/pages/Products/Main/Products.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Cards from "../../../components/Utils/Cards/Cards";
 import PaginationComponent from "../../../components/Utils/PaginationComponent/PaginationComponent";
+import Loading from "../../Loading/Loading";
 import ProductsHeader from "../ProductsHeader/ProductsHeader";
 
 function Products() {
@@ -19,24 +20,28 @@ function Products() {
       })
       .catch((err) => console.log(err));
   }, [breed]);
-  return (
-    <>
-      <Box>
-        <ProductsHeader />
-        <Box sx={{ mb: "1rem", width: "100%", px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem", flexWrap: "wrap" }}>
-          <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-          <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-          <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-          <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-          <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-          <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <>
+        <Box>
+          <ProductsHeader />
+          <Box sx={{ mb: "1rem", width: "100%", px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem", flexWrap: "wrap" }}>
+            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
+            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
+            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
+            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
+            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
+            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
+          </Box>
+          <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", mb: "1rem" }}>
+            <PaginationComponent page={10} />
+          </Box>
         </Box>
-        <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", mb: "1rem" }}>
-          <PaginationComponent page={10} />
-        </Box>
-      </Box>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default Products;
