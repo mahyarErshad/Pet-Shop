afc7d899b4d671f53fd73cace10f58b8fa536eb0
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-12 21:23:00 +0430

    Product page creates cards using database

--- src/pages/Products/Main/Products.tsx
+++ src/pages/Products/Main/Products.tsx
@@ -8,18 +8,29 @@ import ProductsHeader from "../ProductsHeader/ProductsHeader";
 
 function Products() {
   document.title = "محصولات | پت شاپ فینیکس";
-  const breed = useParams().breed;
-  const [products, setProducts] = useState([]);
+  const { breed } = useParams<{ breed: string }>();
   const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState<any>([]);
+
   useEffect(() => {
-    fetch("/api/products")
-      .then((res) => res.json())
-      .then((data) => {
-        setProducts(data);
-        setLoading(false);
-      })
-      .catch((err) => console.log(err));
+    if (breed)
+      fetch("/api/products/" + breed)
+        .then((res) => res.json())
+        .then((data) => {
+          setProducts(data.products);
+          console.log(products);
+          setLoading(false);
+        });
+    else
+      fetch("/api/products")
+        .then((res) => res.json())
+        .then((data) => {
+          setProducts(data.products);
+          console.log(products);
+          setLoading(false);
+        });
   }, [breed]);
+
   if (loading) {
     return <Loading />;
   } else {
@@ -27,14 +38,7 @@ function Products() {
       <>
         <Box>
           <ProductsHeader />
-          <Box sx={{ mb: "1rem", width: "100%", px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem", flexWrap: "wrap" }}>
-            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-            <Cards title="غذای خشک گربه پرشین رویال کنین" image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" price={1200000} />
-          </Box>
+          <Box sx={{ mb: "1rem", width: "100%", px: "1rem", display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem", flexWrap: "wrap" }}>{products && products.map((product: any) => <Cards key={product.id} title={product.title} image={product.image} price={product.price} />)}</Box>
           <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", mb: "1rem" }}>
             <PaginationComponent page={10} />
           </Box>
