a6c9b7c1f0237038f24905dac26c6a1bd4349503
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-13 02:12:29 +0430

    Defined IProduct interface

---
+++ src/Types/types.ts
@@ -0,0 +1,13 @@
+export interface IProduct {
+  id: number;
+  title: string;
+  image: string;
+  brand: string;
+  breed: "سگ" | "گربه" | "پرندگان" | "جوندگان";
+  country: string;
+  weight: number;
+  description: string;
+  category: string;
+  price: number;
+  quantity: number;
+}
