c268e389b07bf7e6749bb925a1ac534671933f01
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-13 02:35:42 +0430

    Working the same with IProduct interface

--- src/pages/SingleProduct/SingleProduct.tsx
+++ src/pages/SingleProduct/SingleProduct.tsx
@@ -5,13 +5,14 @@ import CustomizedTables from "../../components/Utils/SingleProducts/CustomizedTa
 import SingleProductImage from "../../components/Utils/SingleProducts/SingleProductImage/SingleProductImage";
 import SingleProductsDescription from "../../components/Utils/SingleProducts/SingleProductsDescription/SingleProductsDescription";
 import SingleProductsDetails from "../../components/Utils/SingleProducts/SingleProductsDetails/SingleProductsDetails";
+import { IProduct } from "../../Types/types";
 import Loading from "../Loading/Loading";
 import NotFound from "../NotFound/NotFound";
 function SingleProduct() {
   const { id } = useParams();
   const [loading, setLoading] = useState<boolean>(true);
   const [notFound, setNotFound] = useState<boolean>(true);
-  const [product, setProduct] = useState<any>([]);
+  const [product, setProduct] = useState<IProduct>();
 
   useEffect(() => {
     if (id) {
@@ -36,13 +37,13 @@ function SingleProduct() {
     <>
       <Box sx={{ width: "100%", mb: "1rem", display: "flex", flexDirection: "column", px: { xs: "0.5rem", md: "1rem", alignItems: "center", justifyContent: "center", gap: "0.5rem" } }}>
         <Box sx={{ width: "100%", display: "flex", flexDirection: "row-reverse", alignItems: { xs: "center", md: "flex-start" }, justifyContent: { xs: "center", md: "flex-start" }, flexWrap: "wrap", borderBottom: "2px solid #ECEFF1", pb: "0.15rem" }}>
-          <SingleProductImage image={product?.image} />
+          <SingleProductImage image={product!.image} />
           <Box dir="rtl" sx={{ width: { xs: "90%", md: "48%" }, minHeight: "15.625rem", p: "1rem", display: "flex", justifyContent: "flex-start", alignItems: { xs: "center", md: "flex-start" }, flexDirection: "column", gap: "1rem" }}>
-            <SingleProductsDetails title={product?.title} price={product?.price} balance={product?.quantity} fontSize="3rem" />
-            <CustomizedTables weight={product?.weight} country={product?.country} brand={product?.brand} breed={product?.breed} />
+            <SingleProductsDetails title={product!.title} price={product!.price} balance={product!.quantity} fontSize="3rem" />
+            <CustomizedTables weight={product!.weight} country={product!.country} brand={product!.brand} breed={product!.breed} />
           </Box>
         </Box>
-        <SingleProductsDescription description={product?.description} />
+        <SingleProductsDescription description={product!.description} />
       </Box>
     </>
   );
