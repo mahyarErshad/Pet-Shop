bdab6460cb21bba09dee286b0ebe19072ca93873
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-13 00:16:30 +0430

    Fixed some bugs

--- src/pages/SingleProduct/SingleProduct.tsx
+++ src/pages/SingleProduct/SingleProduct.tsx
@@ -18,7 +18,8 @@ function SingleProduct() {
         .then((res) => res.json())
         .then((data) => setProduct(data.product))
         .then(() => setLoading(false))
-        .catch((error) => console.log(error));
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
     }
   }, [id]);
 
@@ -28,13 +29,13 @@ function SingleProduct() {
     return (
       <Box sx={{ width: "100%", mb: "1rem", display: "flex", flexDirection: "column", px: { xs: "0.5rem", md: "1rem", alignItems: "center", justifyContent: "center", gap: "0.5rem" } }}>
         <Box sx={{ width: "100%", display: "flex", flexDirection: "row-reverse", alignItems: { xs: "center", md: "flex-start" }, justifyContent: { xs: "center", md: "flex-start" }, flexWrap: "wrap", borderBottom: "2px solid #ECEFF1", pb: "0.15rem" }}>
-          <SingleProductImage image={product?.image} />
+          <SingleProductImage image={product.image} />
           <Box dir="rtl" sx={{ width: { xs: "90%", md: "48%" }, minHeight: "15.625rem", p: "1rem", display: "flex", justifyContent: "flex-start", alignItems: { xs: "center", md: "flex-start" }, flexDirection: "column", gap: "1rem" }}>
-            <SingleProductsDetails title={product?.title} price={product?.price} balance={product?.quantity} fontSize="3rem" />
-            <CustomizedTables weight={product?.weight} country={product?.country} brand={product?.brand} breed={product?.breed} />
+            <SingleProductsDetails title={product.title} price={product.price} balance={product.quantity} fontSize="3rem" />
+            <CustomizedTables weight={product.weight} country={product.country} brand={product.brand} breed={product.breed} />
           </Box>
         </Box>
-        <SingleProductsDescription description={product?.description} />
+        <SingleProductsDescription description={product.description} />
       </Box>
     );
   }
