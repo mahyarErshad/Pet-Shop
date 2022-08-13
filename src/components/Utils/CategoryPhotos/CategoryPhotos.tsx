4d815be692c820b1cc1a5c63f657d8327d9acde7
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-13 22:33:02 +0430

    Improved Link ability

--- src/components/Utils/CategoryPhotos/CategoryPhotos.tsx
+++ src/components/Utils/CategoryPhotos/CategoryPhotos.tsx
@@ -6,7 +6,7 @@ import { goToTop } from "../../../functions/functions";
 
 interface Iprops {
   title: string;
-  linkTo: string;
+  breed: string;
   image: string;
   animationDuration: string;
 }
@@ -20,14 +20,14 @@ function CategoryPhotos(props: Iprops) {
       fontSize: 11,
     },
   }));
-  const { title, linkTo, image, animationDuration } = props;
+  const { title, breed, image, animationDuration } = props;
   const photoStyle = { height: { xs: "15.625rem", md: "31.25rem" }, width: { xs: "100%", sm: "50%", md: "50%", lg: "25%" }, cursor: "pointer", animationName: { lg: "cardAnimation", md: "" } };
 
   return (
     <>
       <LightTooltip title={title} TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} followCursor>
         <Box onClick={() => goToTop()} sx={{ ...photoStyle, animationDuration: `${animationDuration}` }}>
-          <Link to={linkTo}>
+          <Link to={`/products/category/${breed}`}>
             <Box sx={{ width: "100%", height: "100%", "&:hover": { borderRadius: "16px", border: "2px solid #FFB929", zIndex: "2", transform: "scale(1.1,1.1)", transition: "all 0.3s " } }} component="img" src={image} alt={title} />
           </Link>
         </Box>
