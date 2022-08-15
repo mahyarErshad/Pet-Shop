978fe2f199e8982d74ad424639bb37c62c5555cc
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-15 20:20:17 +0430

    Hardcoded cartItems

--- src/redux/slice/cartReducer.ts
+++ src/redux/slice/cartReducer.ts
@@ -8,7 +8,63 @@ interface cartState {
 }
 
 const initialState: cartState = {
-  cartItems: [],
+  cartItems: [
+    {
+      id: 95,
+      title: "ژل دندان سگ تریکسی",
+      image: "https://rabinseh.com/wp-content/uploads/2022/03/%D8%B2%D9%84-510x510.jpg",
+      brand: "تریکسی",
+      breed: "سگ",
+      country: "آلمان",
+      weight: 100,
+      description: `
+      سلامت دندان و دهان بر سلامت عمومی بدن حیوان بسیار تاثیر گذار است.
+  
+      این ژل سبب تمیز شدن دندان ها و از بین رفتن جرم ها خواهد شد.
+      
+      طعم محصول بسیار دلپذیر است و این امر موجب می شود با مقاومت حیوان در برابر استفاده، مواجه نشوید.
+      
+      روزی یک بار از این ژل به دندان های حیوان زده و به آرامی کمی آن را بمالید.
+      
+      در صورتی که مقداری از ژل توسط سگ یا گربه شما خورده شد، اصلا نگران نباشید.
+      `,
+      category: "بهداشتی سگ",
+      price: 330000,
+    },
+    {
+      id: 96,
+      title: "مسواک دو طرفه ست ۲ عددی",
+      image: "https://rabinseh.com/wp-content/uploads/2022/08/%D9%88%DB%8C%DA%98%DA%AF%DB%8C-%D9%85%D8%B3%D9%88%D8%A7%DA%A9-510x510.jpg",
+      brand: "متفرقه",
+      breed: "سگ",
+      country: "چین",
+      weight: 30,
+      description: `
+      مسواک مخصوص سگ
+      پک دو عددی
+      دارای دو سر جهت مسواک کردن دندان نیش و اسیاب
+      `,
+      category: "بهداشتی سگ",
+      price: 65000,
+    },
+    {
+      id: 97,
+      title: "مسواک انگشتی سگ و گربه بکر",
+      image: "https://rabinseh.com/wp-content/uploads/2021/09/maya-baby-finger-brush-364353829901-02-510x510.jpg",
+      brand: "بکر",
+      breed: "سگ",
+      country: "ایران",
+      weight: 5,
+      description: `
+      • ابعاد: 6*1 سانتی متر
+      • مناسب سگ و گربه
+      • دارای مجوز بهداشت
+      • مقاوم در برابر گاز گرفتن
+      `,
+      category: "بهداشتی سگ",
+      price: 10000,
+    },
+  ],
   total: 0,
   amount: 0,
   isLoading: false,
@@ -17,12 +73,6 @@ const initialState: cartState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {
-    addToCart: (state, action) => {
-      state.cartItems.push(action.payload);
-      state.total += action.payload.price;
-      state.amount += 1;
-    },
-  },
+  reducers: {},
 });
 export default cartSlice.reducer;
