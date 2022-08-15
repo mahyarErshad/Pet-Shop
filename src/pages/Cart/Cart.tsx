60cfaf955b7b5df05a598be4c76aa31a5baa2c57
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-15 22:14:57 +0430

    Defined action for cartItems > 0

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -20,9 +20,9 @@ function Cart() {
       <>
         <Box sx={{ width: "100%", px: "1rem", display: "flex", flexDirection: "column", gap: "1rem", my: "1rem" }}>
           <CartOperationSection />
-          <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />
-          <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />
-          <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />
+          {cartItems.map((item: any) => {
+            return <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />;
+          })}
         </Box>
       </>
     );
