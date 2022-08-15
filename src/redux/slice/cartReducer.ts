cd934cd9306739a7a1d940df3477073a2030ab8f
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-15 23:05:44 +0430

    Added function increaseAmount

--- src/redux/slice/cartReducer.ts
+++ src/redux/slice/cartReducer.ts
@@ -77,6 +77,10 @@ const cartReducer = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    increaseAmount: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
   },
 });
 export const { clearCart } = cartReducer.actions;
