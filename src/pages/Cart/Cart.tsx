b4f399c1d5079f2fa35230999210c359300f0a56
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-15 22:18:37 +0430

    Giving key to cartItem

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -21,7 +21,7 @@ function Cart() {
         <Box sx={{ width: "100%", px: "1rem", display: "flex", flexDirection: "column", gap: "1rem", my: "1rem" }}>
           <CartOperationSection />
           {cartItems.map((item: any) => {
-            return <CartItem image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />;
+            return <CartItem key={item.id} image="https://rabinseh.com/wp-content/uploads/2022/06/4-dgdd-247x247.jpg" title="غذای خشک گربه پرشین رویال کنین" price={1200000} />;
           })}
         </Box>
       </>
