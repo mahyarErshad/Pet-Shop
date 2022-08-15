27c57949a6299d52eea32d2716a7a1e54ffefb5c
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-15 23:07:37 +0430

    Added decreaseAmount function

--- src/redux/slice/cartReducer.ts
+++ src/redux/slice/cartReducer.ts
@@ -81,6 +81,10 @@ const cartReducer = createSlice({
       const cartItem = state.cartItems.find((item) => item.id === payload.id);
       cartItem.amount = cartItem.amount + 1;
     },
+    decreaseAmount: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 export const { clearCart } = cartReducer.actions;
