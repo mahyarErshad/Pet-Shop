b92ae5294e295d87fb7bfed836d766aadbfd0651
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-16 00:27:21 +0430

    Getting amount from cartItems

--- src/components/Utils/Buttons/InDeButton/InDeButton.tsx
+++ src/components/Utils/Buttons/InDeButton/InDeButton.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
 import { Box, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { decreaseAmount, increaseAmount } from "../../../../redux/slice/cartReducer";
 
 interface props {
   width?: string;
@@ -15,16 +17,22 @@ function InDeButton(props: props) {
   const screen = width || "2.5612rem";
   const buttons = width || "";
   const buttonsStyle = { width: buttons, fontSize, height };
+  const dispatch = useDispatch();
+  const amount = useSelector((state: any) => state.cart.cartItems.find((item: any) => item.id === id)?.amount) || 0;
   return (
     <>
       <ButtonGroup dir="rtl" disableElevation variant="contained" color="secondary">
-        <Button sx={buttonsStyle}>+</Button>
+        <Button onClick={() => dispatch(increaseAmount({ id }))} sx={buttonsStyle}>
+          +
+        </Button>
         <Box sx={{ width: screen, height, backgroundColor: "#ECEFF1", display: "flex", justifyContent: "center", alignItems: "center", borderTop: "1px solid #000", borderBottom: "1px solid #000" }}>
           <Typography sx={{ fontSize }} variant="h5" color="#000">
             ۰
           </Typography>
         </Box>
-        <Button sx={buttonsStyle}>-</Button>
+        <Button onClick={() => dispatch(decreaseAmount({ id }))} sx={buttonsStyle}>
+          -
+        </Button>
       </ButtonGroup>
     </>
   );
