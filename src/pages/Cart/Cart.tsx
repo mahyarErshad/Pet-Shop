9af1952645a94ca2250cccbe9815cc69d2b29a72
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-15 21:07:46 +0430

    Imported store

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 import CartItem from "../../components/Utils/CartItem/CartItem";
 import CartOperationSection from "./CartOperationSection";
 
 function Cart() {
   document.title = "پت شاپ فینیکس | سبد خرید";
+  const { cartItems, amount, total } = useSelector((state: any) => state.cart);
   return (
     <>
       <Box sx={{ width: "100%", px: "1rem", display: "flex", flexDirection: "column", gap: "1rem", my: "1rem" }}>
