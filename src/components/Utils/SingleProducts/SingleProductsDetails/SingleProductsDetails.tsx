a8ed0f050b3df1c78759f1d47346391d9b4d4e98
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-16 21:16:09 +0430

    Using MyButton instead of InDebutton

--- src/components/Utils/SingleProducts/SingleProductsDetails/SingleProductsDetails.tsx
+++ src/components/Utils/SingleProducts/SingleProductsDetails/SingleProductsDetails.tsx
@@ -1,18 +1,17 @@
 import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 import { separateNumber } from "../../../../functions/functions";
-import InDeButton from "../../Buttons/InDeButton/InDeButton";
+import MyButton from "../../Buttons/MyButton/MyButton";
 interface props {
   title: string;
   price: number;
   balance: number;
-  fontSize?: string;
   id: string | number;
 }
 
 function SingleProductsDetails(props: props) {
   const { title, price, balance, id } = props;
-  const fontSize = props.fontSize || "";
   document.title = props.title;
 
   return (
@@ -26,7 +25,9 @@ function SingleProductsDetails(props: props) {
       <Typography dir="rtl" sx={{ fontSize: "1rem", fontWeight: "normal" }}>
         موجود در انبار : {separateNumber(balance)} عدد
       </Typography>
-      <InDeButton id={id} width="4rem" height="3rem" fontSize={fontSize} />
+      <Box>
+        <MyButton text="افزودن به سبد خرید" />
+      </Box>
     </>
   );
 }
