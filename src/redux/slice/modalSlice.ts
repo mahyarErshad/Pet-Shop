cf39bba15e11837c8081924b576fefdc5288c896
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-16 16:57:28 +0430

    Initialized the slice

---
+++ src/redux/slice/modalSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {},
+});
+export default modalSlice.reducer;
