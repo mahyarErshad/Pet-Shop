7703d96b5753ec4fde5ee420d3224ac4fd4f9f1c
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-17 16:32:53 +0430

    Button type is submit

--- src/pages/Register/Form/Form.tsx
+++ src/pages/Register/Form/Form.tsx
@@ -25,7 +25,7 @@ function Form(props: IProps) {
             پسورد خود را وارد نمایید:
           </Box>
           <TextField error={passwordError} helperText={passwordErrorMessage} onChange={(e) => dispatch(setPasswordValue(e.target.value))} sx={inputStyle} type="password" color="secondary" required id="password" label="password" variant="outlined" />
-          <Button sx={{ p: "0.5rem", width: { lg: "25%", md: "25%", xs: "37%" }, alignSelf: "flex-end" }} variant="contained" color="secondary">
+          <Button type="submit" sx={{ p: "0.5rem", width: { lg: "25%", md: "25%", xs: "37%" }, alignSelf: "flex-end" }} variant="contained" color="secondary">
             <Typography sx={{ textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }}>{buttonText}</Typography>
           </Button>
         </Box>
