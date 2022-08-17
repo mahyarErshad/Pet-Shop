a20d84ed83b4a42a2abf2701643d9bb51bf475e5
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-17 15:14:10 +0430

    Imported loggedIn reducer

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../slice/cartReducer";
+import loginReducer from "../slice/loginReducer";
 import modalSlice from "../slice/modalSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     modal: modalSlice,
+    loggedIn: loginReducer,
   },
 });
 
