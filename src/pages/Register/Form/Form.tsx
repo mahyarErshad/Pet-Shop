d2176630a6203300b6d302677c8b919d52f4a60c
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-17 17:59:36 +0430

    Changed password's label

--- src/pages/Register/Form/Form.tsx
+++ src/pages/Register/Form/Form.tsx
@@ -49,7 +49,7 @@ function Form(props: IProps) {
           </Box>
           <TextField autoComplete="off" error={emailError} helperText={emailErrorMessage} onChange={(e) => dispatch(setEmailValue(e.target.value))} autoFocus sx={inputStyle} type="email" color="secondary" required id="email" label="email" variant="outlined" />
           <Box component="label" htmlFor="password" sx={{ direction: "rtl", fontSize: "0.875rem", cursor: "pointer" }}>
-            پسورد خود را وارد نمایید:
+            رمز عبور خود را وارد نمایید:
           </Box>
           <TextField error={passwordError} helperText={passwordErrorMessage} onChange={(e) => dispatch(setPasswordValue(e.target.value))} sx={inputStyle} type="password" color="secondary" required id="password" label="password" variant="outlined" />
           <Button onClick={(e: any) => handleSubmit(e)} type="submit" sx={{ p: "0.5rem", width: { lg: "25%", md: "25%", xs: "37%" }, alignSelf: "flex-end" }} variant="contained" color="secondary">
