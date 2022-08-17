90ddc8b6dd340a9f1908a56d8405986d3e35c8e5
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-18 00:04:18 +0430

    Defined message for Modal

--- src/redux/slice/modalSlice.ts
+++ src/redux/slice/modalSlice.ts
@@ -2,18 +2,21 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface modal {
   isOpen: boolean;
+  message: string;
 }
 
 const initialState: modal = {
   isOpen: false,
+  message: "",
 };
 
 const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    changeModalState: (state) => {
+    changeModalState: (state, action) => {
       state.isOpen = !state.isOpen;
+      state.message = action.payload;
     },
   },
 });
