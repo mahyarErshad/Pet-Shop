daed2e6a621720f7fa4ab1b1b5e130aa8dab5c3e
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-17 15:45:25 +0430

    Set email and password using states

--- src/pages/Register/Form/Form.tsx
+++ src/pages/Register/Form/Form.tsx
@@ -17,11 +17,11 @@ function Form(props: IProps) {
           <Box component="label" htmlFor="email" sx={{ direction: "rtl", fontSize: "0.875rem", cursor: "pointer" }}>
             ایمیل خود را وارد نمایید:
           </Box>
-          <TextField autoFocus sx={inputStyle} type="email" color="secondary" required id="email" label="email" variant="outlined" />
+          <TextField onChange={(e) => setEmail(e.target.value)} autoFocus sx={inputStyle} type="email" color="secondary" required id="email" label="email" variant="outlined" />
           <Box component="label" htmlFor="password" sx={{ direction: "rtl", fontSize: "0.875rem", cursor: "pointer" }}>
             پسورد خود را وارد نمایید:
           </Box>
-          <TextField sx={inputStyle} type="password" color="secondary" required id="password" label="password" variant="outlined" />
+          <TextField onChange={(e) => setPassword(e.target.value)} sx={inputStyle} type="password" color="secondary" required id="password" label="password" variant="outlined" />
           <Button sx={{ p: "0.5rem", width: { lg: "25%", md: "25%", xs: "37%" }, alignSelf: "flex-end" }} variant="contained" color="secondary">
             <Typography sx={{ textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }}>{buttonText}</Typography>
           </Button>
