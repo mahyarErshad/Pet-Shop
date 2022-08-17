f7b75224ce0025a1908325dd0fea01a46361f25f
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-17 22:22:39 +0430

    All products button has been added and styled

--- src/components/Header/HeaderMenus/HeaderMenus.tsx
+++ src/components/Header/HeaderMenus/HeaderMenus.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import MenuItems from "../../Utils/MenuItems/MenuItems";
 
 // MUI
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 // icons
 import { GiCat } from "react-icons/gi";
 import { GiSittingDog } from "react-icons/gi";
 import { GiHummingbird } from "react-icons/gi";
 import { GiSquirrel } from "react-icons/gi";
-import MyButton from "../../Utils/Buttons/MyButton/MyButton";
+import { Link } from "react-router-dom";
+import "../../../Style/global/style.css";
 interface Iprops {
   flexRow?: boolean;
 }
@@ -19,7 +20,9 @@ function HeaderMenus(props: Iprops) {
   const row = flexDirection ? "row-reverse" : "column";
   return (
     <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: row }}>
-      <MyButton text="همه محصولات" color="primary" linkTo={`/products/${all}`} />
+      <Link className="noDecoration" to={`/products/${all}`}>
+        <Button sx={{ fontSize: "1rem", fontWeight: "bold", color: "#000", height: "3rem", borderRadius: "8px", "&:hover": { bgcolor: "#B2B2B2" } }}>همه محصولات</Button>
+      </Link>
       <MenuItems title="گربه" productCategory={["غذای گربه", "خاک گربه", "بهداشتی گربه"]} icon={<GiCat fontSize={20} />} />
       <MenuItems title="سگ" productCategory={["غذای سگ", "اسباب بازی سگ", "بهداشتی سگ"]} icon={<GiSittingDog fontSize={20} />} />
       <MenuItems title="پرندگان" productCategory={["غذای پرندگان", "لانه پرندگان"]} icon={<GiHummingbird fontSize={20} />} />
