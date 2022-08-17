cf363bfd992619608438699dde5cacc06b2cf523
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-17 14:39:36 +0430

    Added email and password inputs

--- src/pages/Register/Signup/Signup.tsx
+++ src/pages/Register/Signup/Signup.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { FormControl } from "@mui/material";
-import RegisterInputs from "../../../components/Utils/Inputs/RegisterInputs/RegisterInputs";
-import RegisterButton from "../../../components/Utils/Buttons/RegisterButtons/RegisterButton";
+import { Box, FormControl, TextField } from "@mui/material";
 
 function Signup() {
+  const inputStyle = { width: { lg: "50%", md: "50%", xs: "75%" }, mb: "1rem" };
   return (
     <>
       <FormControl sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "1rem" }}>
-        <RegisterInputs focus={true} label="Email" type="text" title="آدرس ایمیل: " />
-        <RegisterInputs label="Password" type="Password" title="گذر واژه: " />
-        <RegisterButton title="عضویت" />
+        <Box sx={{ width: "100%", display: "flex", alignItems: "flex-end", justifyContent: "center", flexDirection: "column", gap: "0.2rem" }}>
+          <Box component="label" htmlFor="email" sx={{ direction: "rtl", fontSize: "0.875rem", cursor: "pointer" }}>
+            ایمیل خود را وارد نمایید:
+          </Box>
+          <TextField autoFocus={true} sx={inputStyle} type="email" color="secondary" required id="email" label="email" variant="outlined" />
+          <Box component="label" htmlFor="password" sx={{ direction: "rtl", fontSize: "0.875rem", cursor: "pointer" }}>
+            پسورد خود را وارد نمایید:
+          </Box>
+          <TextField sx={inputStyle} type="password" color="secondary" required id="password" label="password" variant="outlined" />
+        </Box>
       </FormControl>
     </>
   );
