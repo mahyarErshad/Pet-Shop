2b14f2ab5c9cdb380dbb5c35f60ace8c7f097d34
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-17 16:03:49 +0430

    helperTexts are dynamic

--- src/pages/Register/Form/Form.tsx
+++ src/pages/Register/Form/Form.tsx
@@ -21,11 +21,11 @@ function Form(props: IProps) {
           <Box component="label" htmlFor="email" sx={{ direction: "rtl", fontSize: "0.875rem", cursor: "pointer" }}>
             ایمیل خود را وارد نمایید:
           </Box>
-          <TextField error={emailError} helperText="فرمت ایمیل صحیح نیست" onChange={(e) => setEmail(e.target.value)} autoFocus sx={inputStyle} type="email" color="secondary" required id="email" label="email" variant="outlined" />
+          <TextField error={emailError} helperText={emailErrorText} onChange={(e) => setEmail(e.target.value)} autoFocus sx={inputStyle} type="email" color="secondary" required id="email" label="email" variant="outlined" />
           <Box component="label" htmlFor="password" sx={{ direction: "rtl", fontSize: "0.875rem", cursor: "pointer" }}>
             پسورد خود را وارد نمایید:
           </Box>
-          <TextField error={passwordError} helperText="پسورد نباید کمتر از 8 کاراکتر باشد" onChange={(e) => setPassword(e.target.value)} sx={inputStyle} type="password" color="secondary" required id="password" label="password" variant="outlined" />
+          <TextField error={passwordError} helperText={passwordErrorText} onChange={(e) => setPassword(e.target.value)} sx={inputStyle} type="password" color="secondary" required id="password" label="password" variant="outlined" />
           <Button sx={{ p: "0.5rem", width: { lg: "25%", md: "25%", xs: "37%" }, alignSelf: "flex-end" }} variant="contained" color="secondary">
             <Typography sx={{ textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }}>{buttonText}</Typography>
           </Button>
