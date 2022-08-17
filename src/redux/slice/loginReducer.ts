9129c307a8ad3c076485ffa4fbd0974d869bad1b
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-17 15:12:00 +0430

    Added createSlice

--- src/redux/slice/loginReducer.ts
+++ src/redux/slice/loginReducer.ts
@@ -1,4 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   loggedIn: false,
   userEmail: "",
 };
+
+const loginReducer = createSlice({
+  name: "login",
+  initialState,
+  reducers: {},
+});
+
+export default loginReducer.reducer;
