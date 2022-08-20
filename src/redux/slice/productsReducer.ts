db01b351948bfa57adca2a15deb63b4cf4cade38
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-20 21:17:13 +0430

    Added isFiltered

--- src/redux/slice/productsReducer.ts
+++ src/redux/slice/productsReducer.ts
@@ -1,9 +1,11 @@
 interface ProductsState {
   products: Product[];
   isAny: boolean;
+  isFiltered: boolean;
 }
 
 const initialState = <ProductsState>{
   products: [],
   isAny: false,
+  isFiltered: false,
 };
