1e5890b8f4c7a057993740efbc978d1a0bdc04f7
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-20 21:37:16 +0430

    Added all needed actions

--- src/redux/slice/productsReducer.ts
+++ src/redux/slice/productsReducer.ts
@@ -2,19 +2,60 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface ProductsState {
   products: any[];
-  isAny: boolean;
+  filteredNotFound: boolean;
   isFiltered: boolean;
 }
 
 const initialState: ProductsState = {
   products: [],
-  isAny: false,
+  filteredNotFound: false,
   isFiltered: false,
 };
 
 const productsReducer = createSlice({
   name: "products",
   initialState,
-  reducers: {},
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload;
+    },
+    filterByBrand: (state, action) => {
+      state.products = state.products.filter((product) => product.brand === action.payload);
+      state.isFiltered = true;
+      if (state.products.length === 0) {
+        state.filteredNotFound = true;
+      }
+    },
+    filterByMinPrice: (state, action) => {
+      state.products = state.products.filter((product) => product.price >= action.payload);
+      state.isFiltered = true;
+    },
+    filterByMaxPrice: (state, action) => {
+      state.products = state.products.filter((product) => product.price <= action.payload);
+      state.isFiltered = true;
+    },
+    filterByName: (state, action) => {
+      state.products = state.products.filter((product) => product.name.includes(action.payload));
+      state.isFiltered = true;
+      if (state.products.length === 0) {
+        state.filteredNotFound = true;
+      }
+    },
+    filterByCountry: (state, action) => {
+      state.products = state.products.filter((product) => product.country === action.payload);
+      state.isFiltered = true;
+      if (state.products.length === 0) {
+        state.filteredNotFound = true;
+      }
+    },
+    resetFilter: (state) => {
+      state.products = [];
+      state.filteredNotFound = false;
+      state.isFiltered = false;
+    },
+  },
 });
+
+export const { setProducts, filterByBrand, filterByMinPrice, filterByMaxPrice, filterByName, filterByCountry, resetFilter } = productsReducer.actions;
+
 export default productsReducer.reducer;
