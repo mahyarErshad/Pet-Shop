42f06772f9bd675ed26ce8bac603f93d87d7a3e1
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-21 17:00:40 +0430

    Added 2 more brands for filtering

--- src/pages/Products/ProductsHeader/ProductsHeader.tsx
+++ src/pages/Products/ProductsHeader/ProductsHeader.tsx
@@ -24,7 +24,7 @@ function ProductsHeader() {
     <>
       <Box sx={{ px: "1rem", mt: "1rem", mb: "1.5rem", display: "flex", justifyContent: "center", alignItems: "center", gap: { lg: "13rem", md: "5rem", sm: "1rem", xs: "0.5rem" }, flexWrap: "wrap", flexDirection: { xs: "column-reverse", md: "row" } }}>
         <Box sx={{ display: "flex", gap: "1.5rem", justifyContent: "center", alignItems: "center", flexWrap: "wrap", flexDirection: "row-reverse" }}>
-          <ProductsSelect label="برند" categories={["رویال کنین", "جوسرا", "رفلکس"]} />
+          <ProductsSelect label="برند" categories={["رویال کنین", "جوسرا", "رفلکس", "تریکسی", "بیفار"]} />
           <ProductsSelect label="قیمت" categories={["کمترین", "بیشترین"]} />
           <ProductsSelect label="کشور سازنده" categories={["آلمان", "فرانسه", "ترکیه", "ایران"]} />
         </Box>
