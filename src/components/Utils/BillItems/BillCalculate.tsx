4487b62dff2d9d84b8d0f90ff35eaf479f9849f9
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-23 21:51:35 +0430

    Added second arg

--- src/components/Utils/BillItems/BillCalculate.tsx
+++ src/components/Utils/BillItems/BillCalculate.tsx
@@ -5,9 +5,12 @@ function BillCalculate() {
   return (
     <>
       <Box sx={{ width: { xs: "100%", sm: "100%", md: "70%", lg: "60%" }, display: "flex", flexWrap: "wrap", flexDirection: "row-reverse", justifyContent: "space-between", alignItems: "center", gap: "0.5rem", border: "1px solid #ECEFF1", p: "0.5rem", alignSelf: "center" }}>
-        <Box sx={{ width: "70%", display: "flex", flexDirection: "row-reverse", gap: "0.2rem", justifyContent: "flex-start", alignItems: "center" }}>
+        <Box sx={{ width: "100%", display: "flex", flexDirection: "row-reverse", gap: "2rem", justifyContent: "center", alignItems: "center" }}>
           <Typography dir="rtl" sx={{ fontSize: "1rem", color: "#000", fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
             جمع کل:
+          </Typography>
+          <Typography dir="rtl" sx={{ fontSize: "1rem", color: "#000", fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
+            یک میلیون تومان
           </Typography>
         </Box>{" "}
       </Box>
