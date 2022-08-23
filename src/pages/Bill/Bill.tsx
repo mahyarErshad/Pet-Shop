d676e6bfc0a2f620b7f67d2dda579086c907eaad
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-23 17:23:26 +0430

    Getting cart history is working

--- src/pages/Bill/Bill.tsx
+++ src/pages/Bill/Bill.tsx
@@ -1,22 +1,42 @@
-import React, { useEffect } from "react";
+import { Typography } from "@mui/material";
+import { Box, useTheme } from "@mui/system";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import CartItem from "../../components/Utils/CartItem/CartItem";
+import { persian } from "../../functions/functions";
 
 function Bill() {
+  const theme = useTheme();
   const { id } = useParams();
   const { history } = useSelector((state: any) => state.cart);
+  const [notFound, setNotFound] = useState<boolean>(true);
+  const cartHistory = history.find((product: any) => product.cartID.toString() === id);
+  console.log(cartHistory);
   useEffect(() => {
-    console.log(history);
-    const cartHistory = history.filter((product: any) => product.cartID.toString() === id);
-    console.log(cartHistory);
+    if (cartHistory) {
+      setNotFound(false);
+    } else {
+      setNotFound(true);
+    }
     // eslint-disable-next-line
   }, [id]);
-
-  return (
-    <div>
-      <div>hi</div>
-    </div>
-  );
+  if (notFound) {
+    return <div>notFound</div>;
+  } else {
+    return (
+      <>
+        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", width: "100%", my: "1rem" }}>
+          <Box>
+            <Typography dir="rtl" variant="h5" sx={{ mt: "1rem", textAlign: "center", fontWeight: "normal", fontSize: "1.5rem", color: theme.palette.secondary.main }}>{`سفارش شماره: ${persian(id!)}`}</Typography>
+          </Box>
+          {cartHistory.items.map((item: any) => {
+            return <CartItem id={item.id} key={item.id} image={item.image} title={item.title} price={item.price} />;
+          })}
+        </Box>
+      </>
+    );
+  }
 }
 
 export default Bill;
