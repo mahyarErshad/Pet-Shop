57014e362dbf0b41ce256420e4d8260062ecfc16
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-23 21:59:06 +0430

    Color is dynamic based on discount

--- src/components/Utils/BillItems/BillCalculate.tsx
+++ src/components/Utils/BillItems/BillCalculate.tsx
@@ -9,15 +9,15 @@ interface IProps {
 
 function BillCalculate(props: IProps) {
   const { text, fee, discount } = props;
-  color = discount && fee > 0 ? "#2E7D31" : "#000";
+  const color = discount && fee > 0 ? "#2E7D31" : "#000";
   return (
     <>
       <Box sx={{ width: { xs: "100%", sm: "100%", md: "70%", lg: "60%" }, display: "flex", flexWrap: "wrap", flexDirection: "row-reverse", justifyContent: "space-between", alignItems: "center", gap: "0.5rem", border: "1px solid #ECEFF1", p: "0.5rem", alignSelf: "center" }}>
         <Box sx={{ width: "100%", display: "flex", flexDirection: "row-reverse", gap: "2rem", justifyContent: "center", alignItems: "center" }}>
-          <Typography dir="rtl" sx={{ fontSize: "1rem", color: "#000", fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
+          <Typography dir="rtl" sx={{ fontSize: "1rem", color, fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
             {text}
           </Typography>
-          <Typography dir="rtl" sx={{ fontSize: "1rem", color: "#000", fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
+          <Typography dir="rtl" sx={{ fontSize: "1rem", color, fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
             {separateNumber(fee)} تومان
           </Typography>
         </Box>{" "}
