a0f7cf9538963d5756c1583a072887baee5f6546
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-23 16:14:37 +0430

    Getting cartHistory on load

--- src/pages/Bill/Bill.tsx
+++ src/pages/Bill/Bill.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 function Bill() {
-  return <div>Bill</div>;
+  const { id } = useParams();
+  const { history } = useSelector((state: any) => state.cart);
+  useEffect(() => {
+    console.log(history);
+    const cartHistory = history.filter((product: any) => product.cartID.toString() === id);
+    console.log(cartHistory);
+    // eslint-disable-next-line
+  }, [id]);
+
+  return (
+    <div>
+      <div>hi</div>
+    </div>
+  );
 }
 
 export default Bill;
