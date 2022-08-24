974f69d5045469918f4fd177ff0d2bdbcf1bca9b
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-24 18:08:09 +0430

    Removed some icons

--- src/pages/AdminDashboard/AdminDashboard.tsx
+++ src/pages/AdminDashboard/AdminDashboard.tsx
@@ -81,17 +81,6 @@ function AdminDashboard() {
               </ListItem>
             ))}
           </List>
-          <Divider />
-          <List>
-            {["All mail", "Trash", "Spam"].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
         </Drawer>
       </Box>
     </>
