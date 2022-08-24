002a79153046f5366bc540768bea40b7b7828f1a
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-25 00:33:30 +0430

    Added 3 test users

--- src/mirage/mockUser.ts
+++ src/mirage/mockUser.ts
@@ -4,6 +4,21 @@ const mockUsers = [
     password: "09120343545",
     cart: [],
   },
+  {
+    email: "user2@gmail.com",
+    password: "09120343545",
+    cart: [],
+  },
+  {
+    email: "user3@gmail.com",
+    password: "09120343545",
+    cart: [],
+  },
+  {
+    email: "user4@gmail.com",
+    password: "09120343545",
+    cart: [],
+  },
 ];
 
 export default mockUsers;
