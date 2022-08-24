0ddedd9043c49f763dd796b4f72959df7520e41e
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-24 17:30:03 +0430

    Dashboard will not be shown to you if you are not an admin

--- src/pages/AdminDashboard/AdminDashboard.tsx
+++ src/pages/AdminDashboard/AdminDashboard.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function AdminDashboard() {
+  const { userEmail } = useSelector((state: any) => state.loggedIn);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (userEmail !== "admin@admin") {
+      navigate("/");
+      const notify = () =>
+        toast.error("شما دسترسی به این بخش ندارید", {
+          position: "top-right",
+          autoClose: 2500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      notify();
+    }
+    // eslint-disable-next-line
+  }, [userEmail]);
   return (
-    <div>
+    <>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl pauseOnFocusLoss draggable pauseOnHover />
       <div>AdminDashboard</div>
-    </div>
+    </>
   );
 }
 
