9973d0188a40ae675f1e24fddc049de407af2656
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-25 17:24:10 +0430

    DashboardProducts is done

--- src/pages/AdminDashboard/DashboardProducts.tsx
+++ src/pages/AdminDashboard/DashboardProducts.tsx
@@ -1,6 +1,7 @@
-import { Button, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
+import { separateNumber } from "../../functions/functions";
 
 function DashboardProducts() {
   const [products, setProducts] = useState<any[]>([]);
@@ -13,23 +14,26 @@ function DashboardProducts() {
       .catch((error) => console.log(error));
     // eslint-disable-next-line
   }, []);
-  function handleDelete(id: string | number) {
-    const filteredProducts = products.filter((item: any) => item.id !== id);
-    setProducts(filteredProducts);
-  }
   return (
     <>
-      <Box sx={{ display: "flex", flexDirection: "column", overflow: "auto" }}>
+      <Box sx={{ display: "flex", flexDirection: "column", overflow: "scroll", height: "85vh" }}>
         {products.length &&
           products.map((product: any) => {
             return (
-              <Box sx={{ px: "0.5rem", border: "solid 1px #000", height: "3rem", borderRadius: "8px", display: "flex", justifyContent: "flex-start", alignItems: "center", flexDirection: "row-reverse", gap: "3rem" }}>
-                <Typography>{product.title}</Typography>
-                <Typography>{product.price}</Typography>
-                <Box sx={{ display: "flex", gap: ".5rem" }}>
-                  <Button onClick={() => handleDelete(product.id)} sx={{ height: "2.5rem" }} variant="outlined" color="error">
-                    حذف
-                  </Button>
+              <Box key={product.id} sx={{ px: "0.5rem", border: "solid 1px #000", height: "3rem", borderRadius: "8px", display: "flex", justifyContent: "flex-start", alignItems: "center", flexDirection: "row-reverse", gap: "5rem" }}>
+                <Box sx={{ maxWidth: "70%", display: "flex", flexDirection: "row-reverse", gap: "0.2rem", justifyContent: "flex-start", alignItems: "center" }}>
+                  <Box component="img" src={product.image} alt="تصویر محصول" sx={{ width: "3rem", height: "3rem", borderRadius: "50%" }} />
+                  <Typography dir="rtl" variant="h5" sx={{ fontSize: "1rem", color: "#000", fontWeight: "normal", textOverflow: "ellipsis", overflow: "hidden" }}>
+                    {product.title}
+                  </Typography>
+                </Box>
+                <Box sx={{ width: "25%", display: "flex", flexDirection: { xs: "column-reverse", sm: "row-reverse" }, gap: "4rem", justifyContent: { xs: "center", sm: "flex-start" }, alignItems: "center" }}>
+                  <Typography dir="rtl" variant="h5" sx={{ fontSize: "1rem", fontWeight: "normal", textAlign: "center", whiteSpace: "nowrap", overflow: "hidden" }}>
+                    {separateNumber(product.balance)} عدد
+                  </Typography>
+                  <Typography dir="rtl" variant="h5" sx={{ fontSize: "1rem", fontWeight: "normal", textAlign: "center", whiteSpace: "nowrap", overflow: "hidden" }}>
+                    {separateNumber(product.price)} تومان
+                  </Typography>
                 </Box>
               </Box>
             );
