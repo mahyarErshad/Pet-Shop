1b6c75e73229245f2887c250708506f41242d73d
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-25 16:49:44 +0430

    Changed passwords

--- src/mirage/mockUser.ts
+++ src/mirage/mockUser.ts
@@ -1,27 +1,27 @@
 const mockUsers = [
   {
     email: "mock.user.number1@gmail.com",
-    password: "09120343545",
+    password: "12345678",
     cart: [],
   },
   {
     email: "mock.user.number2@gmail.com",
-    password: "09120343545",
+    password: "12345678",
     cart: [],
   },
   {
     email: "mock.user.number3@gmail.com",
-    password: "09120343545",
+    password: "12345678",
     cart: [],
   },
   {
     email: "mock.user.number4@gmail.com",
-    password: "09120343545",
+    password: "12345678",
     cart: [],
   },
   {
     email: "mock.user.number5@gmail.com",
-    password: "09120343545",
+    password: "12345678",
     cart: [],
   },
 ];
