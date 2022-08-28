5b73d14eb6788e91a623d7f2721b2557ec23023c
mahyarErshad <mahyar.ershad92@gmail.com>
2022-08-28 15:51:54 +0430

    Added pagination for products

--- src/pages/AdminDashboard/DashboardProducts.tsx
+++ src/pages/AdminDashboard/DashboardProducts.tsx
@@ -1,24 +1,36 @@
-import { Typography } from "@mui/material";
+import { Pagination, Stack, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
-import { separateNumber } from "../../functions/functions";
+import { goToTop, separateNumber } from "../../functions/functions";
+import usePagination from "../../functions/Pagination";
 
 function DashboardProducts() {
   const [products, setProducts] = useState<any[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const PER_PAGE = 6;
+  const count = Math.ceil(products.length / PER_PAGE);
+  const _DATA = usePagination(products, PER_PAGE);
   useEffect(() => {
     fetch("/api/products/all")
       .then((res) => res.json())
       .then((data) => {
         setProducts(data.products);
+        _DATA.reset();
+        setPage(1);
       })
       .catch((error) => console.log(error));
     // eslint-disable-next-line
   }, []);
+  const handleChange = (e: any, p: number) => {
+    setPage(p);
+    _DATA.jump(p);
+    goToTop();
+  };
   return (
     <>
       <Box sx={{ display: "flex", flexDirection: "column", overflow: "scroll", height: "85vh" }}>
         {products.length &&
-          products.map((product: any) => {
+          _DATA.currentData().map((product: any) => {
             return (
               <Box sx={{ width: { xs: "100%", sm: "100%", md: "70%", lg: "60%" }, display: "flex", flexWrap: "wrap", flexDirection: "row-reverse", justifyContent: "space-between", alignItems: "center", gap: "0.5rem", borderRadius: "16px", border: "1px solid #ECEFF1", p: "0.5rem", alignSelf: "center" }}>
                 <Box sx={{ width: { xs: "50%", sm: "40%", md: "50%" }, display: "flex", flexDirection: "row-reverse", gap: "0.2rem", justifyContent: "flex-start", alignItems: "center" }}>
@@ -38,6 +50,11 @@ function DashboardProducts() {
               </Box>
             );
           })}
+        <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", mb: "1rem" }}>
+          <Stack spacing={2}>
+            <Pagination size="large" color="secondary" page={page} count={count} onChange={handleChange} />
+          </Stack>
+        </Box>
       </Box>
     </>
   );
